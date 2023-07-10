bbfb450849d663f688985ce882eaa4ff99c4d95a
MdJakiulRashidKhan <jakiul15-9554@diu.edu.bd>
2023-07-10 17:38:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+function Header({ title }) {
+  return <h1>{title}</h1>;
+}
+
+function Content() {
+  const [text, setText] = useState('');
+  const [displayedText, setDisplayedText] = useState('');
+
+  const handleButtonClick = () => {
+    setDisplayedText(text);
+  };
+
+  const handleTextAreaChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <textarea rows="4" cols="50" onChange={handleTextAreaChange} />
+      <br />
+      <button onClick={handleButtonClick}>Display Text</button>
+      <p>{displayedText}</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>Albert Einstein: "Imagination is more important than knowledge."</footer>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <Header title="My React Application" />
+      <Content />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
